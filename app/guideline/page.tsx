ddc6b9e3c269f042e9814a815a68ac9dfb6e899d
adenkesuma <adenfdfd10@gmail.com>
2023-10-16 15:06:11 +0700

    `feat:` add file guideline

---
+++ app/guideline/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Guideline = () => {
+  return (
+    <main className='min-h-screen pt-12'>
+      <h1 className='text-[38px] font-bold text-gray-800'>Panduan Pelatihan</h1>
+    </main>
+  )
+}
+
+export default Guideline
