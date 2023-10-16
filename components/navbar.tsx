9adc8dbf8521fb526fd2c93a370aab37e7bcedde
adenkesuma <adenfdfd10@gmail.com>
2023-10-16 14:24:35 +0700

    add notification file

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -18,7 +18,7 @@ const Navbar: FC = () => {
             {pathname === "/register" || pathname === "/login" || pathname === "/update_data" ? 
                 <span />
                 : 
-                <nav className="py-4 bg-white">
+                <nav className="py-4">
                     <div className="flex justify-between items-center">
                         <Link href="/">
                             <Image 
