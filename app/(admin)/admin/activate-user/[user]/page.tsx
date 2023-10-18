db8222df19918e41b009222d50e1c93de9af63c6
adenkesuma <adenfdfd10@gmail.com>
2023-10-18 12:43:24 +0700

    finishd activate user detail

--- app/(admin)/admin/activate-user/[user]/page.tsx
+++ app/(admin)/admin/activate-user/[user]/page.tsx
@@ -8,17 +8,30 @@ const ActivateUserDetail = () => {
         <h1 className="font-bold text-[38px] text-gray-800">Aktivasi Sekarang</h1>
 
         <div className='mt-8 gap-10 pt-8 px-8 pb-8 rounded-2xl bg-white'>
-            <div className='flex justify-between gap-2 items-start flex-row w-full'>
-                <h2 className=" classname
-                flex flex-col gap-2 text-sm font-semibod text-gray-600">
-                    <span>Nama Lengkap: </span>
-                    <span className='font-bold text-base text-green'>Abdul Salam</span>
-                </h2>
-                <h2 className=" classname
-                flex flex-col gap-2 text-sm font-semibod text-gray-600">
-                    <span>Nama Lengkap Beserta Gelar:</span> 
-                    <span className='text-base font-bold text-green'>Abdul Salam Almansyur S.Kom S.pdi</span>
-                </h2>
+            <div>
+                <div className="flex flex-row justify-between items-center">
+                    <h2 className=" classname
+                    flex flex-col gap-2 text-sm font-semibod text-gray-600">
+                        <span>Nama Lengkap: </span>
+                        <span className='font-bold text-base text-green'>Abdul Salam</span>
+                    </h2>
+                    <h2 className=" classname
+                    flex flex-col gap-2 text-sm font-semibod text-gray-600">
+                        <span>Nama Lengkap Beserta Gelar:</span> 
+                        <span className='text-base font-bold text-green'>Abdul Salam Almansyur S.Kom S.pdi</span>
+                    </h2>
+                </div>
+
+                <div className="mt-8">
+                    <div className='flex flex-col gap-2'>
+                        <label className='font-medium text-sm'>Pesan Untuk Member Pelatihan <span className="text-red-600">*</span></label>
+                        <textarea rows={6} cols={50} className="border rounded-xl p-2 border-opacity-green"></textarea>
+                    </div>
+
+                    <button className="flex justify-center items-center gap-4 mt-12 text-center w-[240px] text-white font-medium p-2 rounded-xl bg-green">
+                        Aktivasi User
+                    </button>
+                </div>
             </div>
         </div>
 
@@ -146,6 +159,26 @@ const ActivateUserDetail = () => {
                           <td className="whitespace-nowrap px-6 py-4 w-2/4">
                             <div className="flex flex-col gap-2">
                                 <span className="text-sm">Foto KTP</span>
+                                <Image 
+                                    src={Sip}
+                                    alt="foto sip"
+                                    className="w-52 bg-cover bg-bottom object-cover"
+                                />
+                            </div>
+                          </td>
+                        </tr>  
+
+                        <tr
+                          className="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:hover:bg-gray-100">
+                          <td className="whitespace-nowrap px-6 py-4 font-medium w-2/4">
+                            <div className="flex flex-col gap-2">
+                                <span className="text-sm">No ATM:</span>
+                                <span className="text-base text-green font-semibold">213131313123132</span>
+                            </div>
+                          </td>
+                          <td className="whitespace-nowrap px-6 py-4 w-2/4">
+                            <div className="flex flex-col gap-2">
+                                <span className="text-sm">Foto Bukti Pembayaran</span>
                                 <Image 
                                     src={Sip}
                                     alt="foto sip"
