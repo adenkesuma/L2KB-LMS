2f15d0d7e0f24d68aef489e74ee4dc9bae5b5002
adenkesuma <adenfdfd10@gmail.com>
2023-10-19 13:00:47 +0700

    fix any bug in courses detail and admin courses

--- app/(user)/history/page.tsx
+++ app/(user)/history/page.tsx
@@ -2,6 +2,9 @@ import Card from "@/components/card"
 import Link from "next/link"
 
 const History = () => {
+  const text = "Skrining atau uji saring pada bayi baru lahir Neonatal Screening adalah istilah yang sering kita dengar, dan"
+  const shortText = text.slice(0, 50)
+
   return (
     <main className='pt-12 min-h-screen'>
       <div className="flex justify-between items-end">
@@ -32,9 +35,41 @@ const History = () => {
           </div>
       </div>
 
-      <div className="mt-12 grid grid-cols-4 gap-8">
-        <Card />
-      </div>
+      <div className='rounded-2xl border border-gray-100 mt-8 bg-white p-4'>
+            <div className="flex flex-col">
+              <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+                  <div className="overflow-hidden">
+                    <table className="min-w-full text-left text-sm font-light">
+                      <thead className="border-b font-medium text-green text-base">
+                        <tr>
+                          <th scope="col" className="px-6 py-4">No</th>
+                          <th scope="col" className="px-6 py-4">Nama Pelatihan</th>
+                          <th scope="col" className="px-6 py-4">Deskripsi</th>
+                          <th scope="col" className="px-6 py-4">Tanggal Selesai</th>
+                          <th scope="col" className="px-6 py-4">Lokasi Pelatihan</th>
+                          <th scope="col" className="px-6 py-4">Status</th>
+                        </tr>
+                      </thead>
+                      <tbody className="text-gray-600 text-sm font-normal">
+                        <tr
+                          className="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:hover:bg-gray-100">
+                          <td className="whitespace-nowrap px-6 py-4 font-medium">1</td>
+                          <td className="whitespace-nowrap px-6 py-4">Pelatihan skrining bayi baru lahir, bagi Dokter dan Bidan</td>
+                          <td className="whitespace-nowrap px-6 py-4">{shortText}</td>
+                          <td className="whitespace-nowrap px-6 py-4">23 - Oktober - 2023</td>
+                          <td className="whitespace-nowrap px-6 py-4">Belawan</td>
+                          <td className="whitespace-nowrap px-6 py-4">
+                            <span className="py-2 px-4 rounded-xl bg-opacity-green text-green font-semibold">Selesai</span>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
     </main>
   )
 }
