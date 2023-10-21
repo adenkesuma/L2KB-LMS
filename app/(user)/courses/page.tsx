dcda30d771e694cc78501eaa2b831ad11f141bd2
adenkesuma <adenfdfd10@gmail.com>
2023-10-21 21:56:31 +0700

    add mobile responsive for courses and guideline page

--- app/(user)/courses/page.tsx
+++ app/(user)/courses/page.tsx
@@ -4,16 +4,16 @@ import Search from "@/public/assets/icons/search.svg"
 
 const Courses = () => {
   return (
-    <main className='pt-12 min-h-screen'>
+    <main className='mt-4 sm:pt-12 min-h-screen'>
         {/* header title pelatihan */}
         <div className='flex flex-col justify-center items-center gap-2'>
-            <h1 className='font-bold text-center text-[38px]'>Agenda Pelatihan</h1>
-            <p className='font-regular text-[16px]'>Temukan pelatihan yang anda inginkan, dan kembangkan terus kemampuan anda</p>
+            <h1 className='font-bold text-center text-xl sm:text-[38px]'>Agenda Pelatihan</h1>
+            <p className='font-regular text-xs px-4 sm:text-base text-center'>Temukan pelatihan yang anda inginkan, dan kembangkan terus kemampuan anda</p>
             <div className="relative">
               <input 
                 type="text" 
                 placeholder="Cari Pelatihan..." 
-                className='mt-4 border border-slate-300 py-2 pl-12 pr-4 rounded-3xl w-[500px] outline-[#015A39]'
+                className='mt-4 border border-slate-300 py-2 pl-12 pr-4 rounded-3xl max-w-xs text-sm md:w-[500px] outline-[#015A39]'
               />
               <Image 
                 src={Search}
@@ -23,7 +23,7 @@ const Courses = () => {
           </div>
         </div>
 
-        <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mt-12">
+        <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-3 sm:gap-8 mt-6 sm:mt-12">
             <Card />
             <Card />
             <Card />
--- app/(user)/guideline/page.tsx
+++ app/(user)/guideline/page.tsx
@@ -3,14 +3,14 @@ import Guide from "@/public/assets/panduan.jpg"
 
 const Guideline = () => {
   return (
-    <main className='min-h-screen pt-12'>
-      <h1 className='text-[38px] font-bold text-gray-800'>Panduan Pelatihan</h1>
+    <main className='min-h-screen pt-4 sm:pt-12'>
+      <h1 className='text-xl sm:text-[38px] font-bold text-gray-800'>Panduan Pelatihan</h1>
 
-      <figure className='mt-12'>
+      <figure className='pt-4 sm:mt-12'>
         <Image 
           src={Guide}
           alt="guideline image"
-          className='w-full'
+          className='w-full rounded-md'
         />
       </figure>
     </main>
