6538bda1746feeef892f33ec2e9d0ee813084db5
adenkesuma <adenfdfd10@gmail.com>
2023-10-22 10:20:25 +0700

    add mobile responsive for certificate user

--- app/(user)/certificate/page.tsx
+++ app/(user)/certificate/page.tsx
@@ -3,34 +3,34 @@ import UserCertificate from "@/components/userCertificate"
 
 const Certificate = () => {
   return (
-     <main className='pt-12 min-h-screen'>
-        <div className="flex justify-between items-end">
+     <main className='pt-4 sm:pt-12 min-h-screen'>
+        <div className="flex justify-between flex-col sm:flex-row items-start sm:items-end">
             <div>
-                <h1 className='text-[38px] font-bold text-gray-800'>Sertifikat Saya</h1>
-                <span className='text-sm text-gray-600'>Unduh sertifikat pelatihan Anda di bawah ini</span>
-                <ul className="flex justify-start items-center gap-12 mt-8">
+                <h1 className='text-xl sm:text-[38px] font-bold text-gray-800'>Sertifikat Saya</h1>
+                <span className='text-xs sm:text-sm text-gray-600'>Unduh sertifikat pelatihan Anda di bawah ini</span>
+                <ul className="flex justify-start items-center gap-6 sm:gap-12 mt-4 sm:mt-8">
                     <li>
-                        <Link className="text-green font-semibold text-md hover:border-b hover:border-green" href="/profile">Profil saya</Link>
+                        <Link className="text-green font-semibold text-xs sm:text-base hover:border-b hover:border-green" href="/profile">Profil saya</Link>
                     </li>
                     <li>
-                        <Link className="text-green font-semibold text-md hover:border-b hover:border-green" href="/my-course">Pelatihan saya</Link>
+                        <Link className="text-green font-semibold text-xs sm:text-base hover:border-b hover:border-green" href="/my-course">Pelatihan saya</Link>
                     </li>
                     <li>
-                        <Link className="text-green font-semibold text-md hover:border-b hover:border-green" href="/history">Riwayat pelatihan saya</Link>
+                        <Link className="text-green font-semibold text-xs sm:text-base hover:border-b hover:border-green" href="/history">Riwayat pelatihan saya</Link>
                     </li>
                 </ul>
             </div>
-            <div className="flex flex-col gap-4">
-                <label className="text-sm text-gray-600">Cari Sertifikat Saya</label>
+            <div className="flex flex-col gap-2 mt-4 sm:mt-0 sm:gap-3">
+                <label className="text-xs sm:text-sm text-gray-600">Cari Sertifikat Saya</label>
                 <input 
                   type="text" 
                   placeholder="Cari..." 
-                  className='border border-opacity-green py-2 px-4 rounded-3xl w-[440px] outline-[#29A398]'
+                  className='border border-opacity-green py-2 px-4 rounded-3xl w-full sm:w-[440px] outline-[#29A398]'
                 />
             </div>
         </div>
 
-        <div className="flex flex-col gap-8 mt-8">
+        <div className="flex flex-col gap-4 sm:gap-8 mt-4 sm:mt-8">
           <UserCertificate />
           <UserCertificate />
           <UserCertificate />
