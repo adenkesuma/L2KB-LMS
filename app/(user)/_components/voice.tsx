78fb1c795941501092748e5df4e96de73daef6b5
adenkesuma <adenfdfd10@gmail.com>
2023-10-26 15:00:07 +0700

    add detail

--- app/(user)/_components/voice.tsx
+++ app/(user)/_components/voice.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 
 function HomeVoice() {
   useEffect(() => {
     const audio = new Audio("/assets/voice/voice-over.mp3");
     audio.autoplay = true;
   }, []);
+
   return null;
 }
 
